14fb2686884359a069c92205c875094f753d07c4
DASU\ldl47 <473613943@qq.com>
2024-01-03 13:27:40 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import Receptionist from "../package/index"
+import Receptionist from '../package/index'
 
 
 const scene = new THREE.Scene()
